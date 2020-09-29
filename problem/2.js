26e9f73f2f4cb50680357a315ced165b679ad7ad
xuguo <xuguo@leimingtech.com>
2020-09-29 18:04:43 +0800

    feat: 链表 加法

--- problem/2.js
+++ problem/2.js
@@ -12,6 +12,7 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
+// 反序
 var addTwoNumbers = function (l1, l2) {
   let hair = new ListNode(null);
   let n = hair;
@@ -30,6 +31,44 @@ var addTwoNumbers = function (l1, l2) {
   }
   if (base) {
     n.next = new ListNode(base);
+  }
+
+  return hair.next;
+};
+
+// 正序
+var addTwoNumbers2 = function (l1, l2) {
+  let stack1 = [],
+    stack2 = [];
+  let l = l1,
+    r = l2;
+  while (l) {
+    stack1.push(l.val);
+    l = l.next;
+  }
+  while (r) {
+    stack2.push(r.val);
+    r = r.next;
+  }
+
+  let base = 0;
+  let resStack = [];
+  while (stack1.length || stack2.length) {
+    let x = stack1.pop() || 0;
+    let y = stack2.pop() || 0;
+    let sum = base + x + y;
+    base = (sum / 10) | 0;
+    resStack.push(sum % 10);
+  }
+  if (base) {
+    resStack.push(base);
+  }
+
+  let hair = new ListNode(null);
+  let n = hair;
+  while (resStack.length) {
+    n.next = new ListNode(resStack.pop());
+    n = n.next;
   }
 
   return hair.next;
